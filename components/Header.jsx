7e4809ca1f8cfe877d9d13fc99a360907d2a44a0
nwebpro <abnaeem.bsc@gmail.com>
2024-04-17 11:30:52 +0600

    movie db website setup done

---
+++ components/Header.jsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Header = () => {
+	return (
+		<header>
+			<nav className="container flex items-center justify-between space-x-10 py-6">
+				<Link href="index.html">
+					<Image src="/logo.svg" width={139} height={26} alt="" />
+				</Link>
+
+				<ul className="flex items-center space-x-5">
+					<li>
+						<Link
+							className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+							href="#"
+						>
+							<Image
+								src="/ring.svg"
+								width={24}
+								height={24}
+								alt=""
+							/>
+						</Link>
+					</li>
+					<li>
+						<Link
+							className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+							href="#"
+						>
+							<Image
+								src="/icons/sun.svg"
+								width={24}
+								height={24}
+								alt=""
+							/>
+						</Link>
+					</li>
+					<li>
+						<Link
+							className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+							href="#"
+						>
+							<Image
+								src="/shopping-cart.svg"
+								width={24}
+								height={24}
+								alt=""
+							/>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
